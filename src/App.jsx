3e4c9e65ae48bf97c76a80f5f694c21949fea175
Emmie <emmanueladeremi512@gmail.com>
2025-08-29 07:44:23 +0100

    fixed a bug

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import Nav from "./COMPONENTS/Nav";
+import Nav from "../src/COMPONENTS/Ui/Nav";
 import About from "./COMPONENTS/About";
 import HealthcareServices from "./COMPONENTS/HealtCareServices";
 import HealthSpecialist from "./COMPONENTS/LOg/HealthSpecialist";
